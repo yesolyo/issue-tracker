f0a9d9a262340f278f90b465d23ac2e9b55bffc5
yesolyo <ahnyelim77@gmail.com>
2023-05-12 11:46:21 +0900

    ADD: reset 환경설정
    - 전체 환경 설정이 필요해서 전역으로 reset 환경설정을 완료하였습니다.

--- FE/src/index.jsx
+++ FE/src/index.jsx
@@ -1,15 +1,13 @@
-import React from 'react';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.js";
 
-import ReactDOM from 'react-dom/client';
-
-import App from './App.js';
-
-if (process.env.NODE_ENV === 'development') {
-  const { worker } = require('./mocks/browser');
+if (process.env.NODE_ENV === "development") {
+  const { worker } = require("./mocks/browser");
   worker.start();
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
