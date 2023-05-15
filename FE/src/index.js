9965ca220dada0d0678a55f9a272d33651fd2d9d
yesolyo <ahnyelim77@gmail.com>
2023-05-15 11:37:42 +0900

    ADD: reset, default style 환경설정 완료(#24)
    - 전에는 전역으로 reset과 default style을 한 파일로 모아서 환경설정을 완료하였으나, 추후 분리 필요성을 느꼈습니다.
    - reset은 react 내부 styled components 기능이 있어서 적용하였습니다.

--- FE/src/index.js
+++ FE/src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 import ReactDOM from 'react-dom/client';
 
-import App from './App.js';
+import App from './App.jsx';
 
 if (process.env.NODE_ENV === 'development') {
-  const { worker } = require('./mocks/browser');
+  const { worker } = require('./mocks/browser.js');
   worker.start();
 }
 
