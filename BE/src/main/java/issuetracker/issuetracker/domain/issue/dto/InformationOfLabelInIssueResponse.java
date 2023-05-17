16a27d59285ccc17a3b01db7d55673d4a885529a
leegyeongwhan <leekhy02@naver.com>
2023-05-18 08:23:03 +0900

    FEAT:  이슈를 도메인에 필요한 DTO들을 구현 (#28)
    - 이슈 안에 있는 작성자,라벨,마일스톤들을 DTOReponse로 구현햇고 이슈들의 List에 이 Dto들의 정보를 담을 생각으로 구현했습니다.

---
+++ BE/src/main/java/issuetracker/issuetracker/domain/issue/dto/InformationOfLabelInIssueResponse.java
@@ -0,0 +1,8 @@
+package issuetracker.issuetracker.domain.issue.dto;
+
+public class InformationOfLabelInIssueResponse {
+
+    private long labelId;
+    private String title;
+    private String backgroundColor;
+}
---
+++ BE/src/main/java/issuetracker/issuetracker/domain/issue/dto/MilestoneResponse.java
@@ -0,0 +1,6 @@
+package issuetracker.issuetracker.domain.issue.dto;
+
+public class MilestoneResponse {
+    private int milestone_id;
+    private String title;
+}
