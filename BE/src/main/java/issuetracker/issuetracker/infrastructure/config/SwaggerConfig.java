487b97868bbee2f75a462a9f209abff112157403
leegyeongwhan <leekhy02@naver.com>
2023-05-17 18:12:30 +0900

    ADD:  Swagger 버전이 일치하지않아 구버전에서 신버전으로 업데이트 (#28)
    - wagger 버전이 일치하지않아 구버전에서 신버전으로 업데이트합니다 기존 spring boot와 충돌이나 openapi메서드를 사용합니다.

--- BE/src/main/java/issuetracker/issuetracker/infrastructure/config/SwaggerConfig.java
+++ BE/src/main/java/issuetracker/issuetracker/infrastructure/config/SwaggerConfig.java
@@ -1,40 +1,27 @@
 package issuetracker.issuetracker.infrastructure.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
 
-    private static final String API_NAME = "UMJJAL Project API";
-    private static final String API_VERSION = "0.0.1";
-    private static final String API_DESCRIPTION = "UMJJAL 프로젝트 명세서";
-
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("team04")
+                .pathsToMatch("/**")
                 .build();
     }
 
-    public ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title(API_NAME)
-                .version(API_VERSION)
-                .description(API_DESCRIPTION)
-                .contact(new Contact("KAFKA", "https://kafcamus.tistory.com/", "dlrjsgml42@naver.com"))
-                .build();
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Issue Tracker API")
+                        .description("Issue Tracker API 명세서입니다.")
+                        .version("v0.0.1"));
     }
 }
