fb411fe40671ac4b99e59dbc4962a6830bd2d66c
birdieHyun <flaehdan@gmail.com>
2023-05-19 20:23:24 +0900

    ADD: CountInfo 분리 (#10)
    MaingPage 전체를 구성하는 정보를 전송했었는데, api 분리를 통해, api당 역할 부담을 줄였습니다.

--- BE/src/main/java/issuetracker/issuetracker/domain/page/PageController.java
+++ BE/src/main/java/issuetracker/issuetracker/domain/page/PageController.java
@@ -1,9 +1,12 @@
 package issuetracker.issuetracker.domain.page;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
-import issuetracker.issuetracker.domain.page.dto.MainPage;
+import issuetracker.issuetracker.domain.issue.dto.req.IssueDTO;
+import issuetracker.issuetracker.domain.page.dto.CountInfo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Tag(name = "posts", description = "issue 메인 API")
 @RestController
@@ -14,7 +17,12 @@ public class PageController {
     private final PageService service;
 
     @GetMapping
-    public MainPage showMainPage() {
+    public List<IssueDTO> showMainPage() {
         return service.getMainPage();
+    }
+
+    @GetMapping("/countInfo")
+    public CountInfo getCountInfo() {
+        return service.getCountInfo();
     }
 }
--- BE/src/main/java/issuetracker/issuetracker/domain/page/PageService.java
+++ BE/src/main/java/issuetracker/issuetracker/domain/page/PageService.java
@@ -3,7 +3,6 @@ package issuetracker.issuetracker.domain.page;
 import issuetracker.issuetracker.domain.issue.dto.req.IssueDTO;
 import issuetracker.issuetracker.domain.issue.repositroy.IssueRepositoryJDBCtem;
 import issuetracker.issuetracker.domain.page.dto.CountInfo;
-import issuetracker.issuetracker.domain.page.dto.MainPage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,21 +16,18 @@ public class PageService {
     private final PageRepository repository;
     private final IssueRepositoryJDBCtem issueRepository;
 
-    public MainPage getMainPage() {
+    public List<IssueDTO> getMainPage() {
 
         // issueList 조회
-        List<IssueDTO> issueList = issueRepository.findAll();
+        return issueRepository.findAll();
+    }
 
-        long openIssueCount = issueList.stream().filter(IssueDTO::getIsOPen).count();
-        long closeIssueCount = issueList.size() - openIssueCount;
-
-        // label count , repository 에서 label count 만 꺼내는 쿼리 작성
-        Long labelCount = repository.countLabel();
-        Long milestoneCount = repository.countMilestone();
-
-        CountInfo countInfo = new CountInfo(labelCount, milestoneCount, openIssueCount, closeIssueCount);
-
-        // 열린이슈  카운트 하기, 닫힌이슈
-        return new MainPage(issueList, countInfo);
+    public CountInfo getCountInfo() {
+        return new CountInfo(
+                repository.countLabel(),
+                repository.countMilestone(),
+                repository.countOpenIssue(),
+                repository.countCloseIssue()
+        );
     }
 }
