d0fea832b03788027f87a7ca5eb8fd0c59e8c490
leegyeongwhan <leekhy02@naver.com>
2023-05-23 13:55:48 +0900

    CHORE: DTO 변경사항 반영 (codesquad-2023-group04#30)
    -

--- BE/src/main/java/issuetracker/issuetracker/domain/page/dto/CountInfo.java
+++ BE/src/main/java/issuetracker/issuetracker/domain/page/dto/CountInfo.java
@@ -2,10 +2,12 @@ package issuetracker.issuetracker.domain.page.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class CountInfo {
     private Long labelCount;
