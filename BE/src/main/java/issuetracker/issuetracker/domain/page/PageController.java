c7aeb4c2b81fa0691a5849076228ef6161761583
leegyeongwhan <leekhy02@naver.com>
2023-05-23 13:49:40 +0900

    FEAT: 필터링된 페이지 기능을 page 모메인에 만듭니다 (codesquad-2023-group04#30)
    - 기존의 이슈리스트와 마찬가지로 issue와는 다르다고 생각했습니다.

--- BE/src/main/java/issuetracker/issuetracker/domain/page/PageController.java
+++ BE/src/main/java/issuetracker/issuetracker/domain/page/PageController.java
@@ -1,9 +1,14 @@
 package issuetracker.issuetracker.domain.page;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import issuetracker.issuetracker.domain.issue.controller.IssueController;
+import issuetracker.issuetracker.domain.page.dto.request.FilterRequestDTO;
 import issuetracker.issuetracker.domain.issue.dto.IssueDTO;
+import issuetracker.issuetracker.domain.issue.service.IssueFilterService;
 import issuetracker.issuetracker.domain.page.dto.CountInfo;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,10 +20,22 @@ import java.util.List;
 public class PageController {
 
     private final PageService service;
+    private final IssueFilterService issueQueryService;
+    private final Logger logger = LoggerFactory.getLogger(IssueController.class);
+
 
     @GetMapping
     public List<IssueDTO> showMainPage() {
         return service.getMainPage();
+    }
+
+    //TODO 이슈 필터링 기능 하는 메서드 처음에는 issue에 만들었지만 여기가 더 맞는것 같다.
+    @GetMapping("/test")
+    public List<IssueDTO> filterIssue(FilterRequestDTO filterRequestDTO) {
+        logger.debug("filterRequestDTO={}", filterRequestDTO);
+        logger.debug("검색어와 상태에 따른 이슈 조회");
+        List<IssueDTO> filteredIssues = issueQueryService.filterIssues(filterRequestDTO);
+        return filteredIssues;
     }
 
     @GetMapping("/countInfo")
---
+++ BE/src/main/java/issuetracker/issuetracker/domain/page/PageFilterService.java
@@ -0,0 +1,32 @@
+package issuetracker.issuetracker.domain.page;
+
+import issuetracker.issuetracker.domain.issue.repositroy.IssueRepository;
+import issuetracker.issuetracker.domain.label.LabelRepository;
+import issuetracker.issuetracker.domain.milestone.MileRepository;
+import issuetracker.issuetracker.domain.user.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class PageFilterService {
+
+    private final IssueRepository issueRepository;
+    private final LabelRepository labelRepository;
+    private final MileRepository mileRepository;
+    private final MemberRepository memberRepository;
+
+
+//    public List<IssueDTO> filterIssueDto() {
+//        Iterable<Issue> all = issueRepository.findAll();
+//        for (Issue issue : all) {
+//            long issueId = issue.getId();
+//            IssueDetail byIssueDetail = issueRepository.findByIssueDetail(issueId);
+//            issueRepository.findByAuthor();
+//            issueRepository.findByAssignees();
+//            labelRepository.findByLabelDto();
+//        }
+//        IssueDTO.of()
+//    }
+}
