067c9cd889855720f81d88a53021ce2b438c5939
leegyeongwhan <leekhy02@naver.com>
2023-05-23 13:47:56 +0900

    FEAT: 이슈 어그리거트 생태 계 구현 (codesquad-2023-group04#30)
    - 이슈와 라벨 관련테이블인 IssueAttachedLabel 을 같은 패키지에 넣는다 이슈DTO를 각각 다른 객체나 데이터를 조립하여 만들기위해 빌더패턴을 사용한다 of메서드를 추가.

--- BE/src/main/java/issuetracker/issuetracker/domain/issue/Issue.java
+++ BE/src/main/java/issuetracker/issuetracker/domain/issue/Issue.java
@@ -1,8 +1,11 @@
 package issuetracker.issuetracker.domain.issue;
 
+import issuetracker.issuetracker.domain.milestone.Milestone;
 import lombok.*;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.jdbc.core.mapping.AggregateReference;
 
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Getter
@@ -16,4 +19,7 @@ public class Issue {
     private LocalDateTime updateTime;
     private Boolean isOPen;
     private Boolean isDelete;
+    //지워야 할수도
+    AggregateReference<Milestone, @NotNull Long> milestoneId;
+
 }
