a1b3c4e97c81d6d98c348b7b91f6630ee034f5da
leegyeongwhan <leekhy02@naver.com>
2023-05-25 17:55:12 +0900

    FEAT: 데이터베이스 스키마, insert문 추가합니다 (codesquad-2023-group04#58)
    - 데이터베이스 스키마와 insert 문을 추가합니다.

--- BE/src/main/resources/data/schma.sql
+++ BE/src/main/resources/data/schma.sql
@@ -1,21 +1,27 @@
+DROP DATABASE issue;
+
+CREATE DATABASE issue;
+
+USE issue;
+
 CREATE TABLE `milestone`
 (
-    `milestone_id` INT         NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    `title`        VARCHAR(45) NOT NULL,
+    `milestone_id` INT          NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `title`        VARCHAR(45)  NOT NULL,
     `description`  VARCHAR(300) NULL,
-    `create_time`  DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `is_delete`    TINYINT     NOT NULL DEFAULT 0
+    `create_time`  DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `is_delete`    BIT          NOT NULL DEFAULT 0
 );
 
 
 
 CREATE TABLE `member`
 (
-    `member_id`   INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `login_id`    VARCHAR(45) NOT NULL UNIQUE,
-    `member_name` VARCHAR(45) NOT NULL,
-    `password`    VARCHAR(45) NOT NULL,
-    `email`       VARCHAR(45) NOT NULL,
+    `member_id`   INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `login_id`    VARCHAR(45)  NOT NULL UNIQUE,
+    `member_name` VARCHAR(45)  NOT NULL,
+    `password`    VARCHAR(45)  NOT NULL,
+    `email`       VARCHAR(45)  NOT NULL,
     `profile_url` VARCHAR(300) NULL
 );
 
@@ -25,12 +31,11 @@ CREATE TABLE `issue`
 (
     `issue_id`     INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `title`        VARCHAR(45) NOT NULL,
-    `contents`     TEXT(30000) NULL,
     `create_time`  DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `update_time`  DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    `is_open`      TINYINT     NOT NULL DEFAULT 1,
-    `is_delete`    TINYINT     NOT NULL DEFAULT 0,
-    `milestone_id` INT NULL,
+    `is_open`      BIT         NOT NULL DEFAULT 1,
+    `is_delete`    BIT         NOT NULL DEFAULT 0,
+    `milestone_id` INT         NULL,
     `author`       INT         NOT NULL,
 
     CONSTRAINT `fk_Issue_Milestone1`
@@ -53,8 +58,9 @@ CREATE TABLE `label`
 (
     `label_id`         INT          NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `title`            VARCHAR(45)  NOT NULL,
-    `description`      VARCHAR(300) NOT NULL,
-    `is_delete`        TINYINT      NOT NULL DEFAULT 0,
+    `font_color`       VARCHAR(45)  NOT NULL,
+    `description`      VARCHAR(300) NULL,
+    `is_delete`        BIT          NOT NULL DEFAULT 0,
     `background_color` VARCHAR(45)  NOT NULL
 );
 
@@ -64,8 +70,8 @@ CREATE TABLE `comment`
 (
     `comment_id`  INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `contents`    VARCHAR(45) NOT NULL,
-    `update_time` DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `create_time` DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `create_time` DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `update_time` DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     `issue_id`    INT         NOT NULL,
     `member_id`   INT         NOT NULL,
 
@@ -85,9 +91,9 @@ CREATE TABLE `comment`
 
 CREATE TABLE `label_list`
 (
+    `label_list_id`  INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `label_id` INT NOT NULL,
     `issue_id` INT NOT NULL,
-    PRIMARY KEY (`label_id`, `issue_id`),
 
     CONSTRAINT `fk_Label_has_Issue_Label1`
         FOREIGN KEY (`label_id`)
@@ -105,9 +111,9 @@ CREATE TABLE `label_list`
 
 CREATE TABLE `assignee`
 (
+    `assignee_id`  INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `member_id` INT NOT NULL,
     `issue_id`  INT NOT NULL,
-    PRIMARY KEY (`member_id`, `issue_id`),
 
     CONSTRAINT `fk_User_has_Issue_User1`
         FOREIGN KEY (`member_id`)
@@ -125,7 +131,7 @@ CREATE TABLE `assignee`
 
 CREATE TABLE `file`
 (
-    `file_id`  INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `file_id`  INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `file_url` VARCHAR(300) NULL
 );
 
@@ -133,9 +139,9 @@ CREATE TABLE `file`
 
 CREATE TABLE `emoji`
 (
-    `emoji_id`     INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `emoji_id`    INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `emoji_shape` VARCHAR(45) NULL,
-    `emoji_count`  INT NULL
+    `emoji_count` INT         NULL
 );
 
 
