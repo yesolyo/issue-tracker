15277609ec32b6af8e93a1098b0fc760d647c234
birdieHyun <flaehdan@gmail.com>
2023-05-25 18:45:46 +0900

    ADD: PR 충돌 해결

--- BE/src/main/java/issuetracker/issuetracker/domain/milestone/MilestoneService.java
+++ BE/src/main/java/issuetracker/issuetracker/domain/milestone/MilestoneService.java
@@ -1,20 +1,28 @@
 package issuetracker.issuetracker.domain.milestone;
 
 import issuetracker.issuetracker.domain.milestone.dto.MileStoneDTO;
-import lombok.AllArgsConstructor;
+import issuetracker.issuetracker.domain.milestone.dto.MilestoneFilterDTO;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.jdbc.core.mapping.AggregateReference;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.validation.constraints.NotNull;
+import java.util.List;
 
 @Service
-@AllArgsConstructor
-public class MileStoneService {
+@RequiredArgsConstructor
+public class MilestoneService {
 
-    final private MileRepository mileRepository;
+    private final MileRepository repository;
 
-    public MileStoneDTO findMilestoneInIssue(AggregateReference<Milestone, @NotNull Long> issue) {
-        return mileRepository.findByMilestoneInIssue(issue.getId());
+    @Transactional(readOnly = true)
+    public List<MilestoneFilterDTO> getMilestoneFilter() {
+        return repository.getMilestoneFilter();
     }
 
+
+    public MileStoneDTO findMilestoneInIssue(AggregateReference<Milestone, @NotNull Long> issue) {
+        return repository.findByMilestoneInIssue(issue.getId());
+    }
 }
