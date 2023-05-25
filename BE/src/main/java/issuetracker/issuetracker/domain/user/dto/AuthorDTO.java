b29b0882827cdbc2393a2c0d618fdb5c0d2e4006
leegyeongwhan <leekhy02@naver.com>
2023-05-25 18:01:10 +0900

    FEAT: 이슈 어그리거트 수정(codesquad-2023-group04#58)
    - 이슈, 이슈 어그리거트에 있는 엔티티들 수정

--- BE/src/main/java/issuetracker/issuetracker/domain/user/dto/AuthorDTO.java
+++ BE/src/main/java/issuetracker/issuetracker/domain/user/dto/AuthorDTO.java
@@ -1,13 +1,16 @@
 package issuetracker.issuetracker.domain.user.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 import org.springframework.data.annotation.Id;
 
 import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
+@ToString
 public class AuthorDTO {
     @Id
     private long id;
