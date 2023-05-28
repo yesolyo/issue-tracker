a7b43554301fca98a25920279ade0c2d968223bd
yesolyo <ahnyelim77@gmail.com>
2023-05-28 12:20:45 +0900

    FEAT: 이슈 작성 페이지에서 띄어 쓰기 표시 변경
    - 기존 띄어쓰기 위치를 text area부분에 표시하였습니다.
    - 글을 작성하고, 2초 뒤에 표시됩니다.
    - 기존에 제목과 코멘트를 입력해야지 완료 버튼이 표시됐지만, 그 점을 변경하여 제목만 입력해도 완료 버튼이 표시 되게 하였습니다.

--- FE/src/assets/svgs/index.js
+++ FE/src/assets/svgs/index.js
@@ -18,3 +18,4 @@ export { ReactComponent as plus } from '../svgs/plus.svg';
 export { ReactComponent as smile } from '../svgs/smile.svg';
 export { ReactComponent as chevronDown } from '../svgs/chevronDown.svg';
 export { ReactComponent as roundImage } from '../svgs/roundImage.svg';
+export { ReactComponent as grip } from '../svgs/grip.svg';
