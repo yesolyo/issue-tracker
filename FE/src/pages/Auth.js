94feeb478fe77a7e58320a24229e6df7031b3eb9
yesolyo <ahnyelim77@gmail.com>
2023-05-31 16:08:50 +0900

    CORRECT: 로그인 get 요청 구조 변경 완료

--- FE/src/pages/Auth.js
+++ FE/src/pages/Auth.js
@@ -2,6 +2,8 @@ import { useEffect } from 'react';
 
 import { useNavigate } from 'react-router-dom';
 
+import { fetchData } from '../utils/fetch';
+
 export const Auth = () => {
   const navigate = useNavigate();
   const AUTH_URI = 'http://13.209.232.172:8080/githublogin';
@@ -11,11 +13,8 @@ export const Auth = () => {
   useEffect(() => {
     const getToken = async () => {
       try {
-        const response = await fetch(`${AUTH_URI}?code=${queryCode}`);
-        const data = await response.json();
-        console.log('data', data);
-
-        localStorage.getItem('loginToken', data.jwt);
+        const response = await fetchData(`${AUTH_URI}?code=${queryCode}`);
+        console.log(response);
 
         // navigate('/issues');
       } catch (error) {
